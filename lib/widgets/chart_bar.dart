968958482300dced888b93ea9ff24754d47af7e2
unknown <orionb.y23@gmail.com>
2021-01-05 02:03:28 +0000

    reversing date on chart

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -11,8 +11,11 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        FittedBox(
-          child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        Container(
+          height: 10,
+          child: FittedBox(
+            child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+          ),
         ),
         SizedBox(height: 4),
         Container(
